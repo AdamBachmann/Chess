c3da1f6a04a995e240da0337ba06aa10b6ec28d7
Adam <adambachmann7@gmail.com>
2024-06-16 13:58:33 +0200

    Added side for piece field

--- piece.hpp
+++ piece.hpp
@@ -4,22 +4,35 @@
 
 typedef enum Piece_type_t {
 	DEFAULT = 'X',
-	PAWN = 'P',
-	KNIGHT = 'N',
-	BISHOP = 'B',
-	ROOK = 'R',
-	QUEEN = 'Q',
-	KING= 'K'
+	PAWN	= 'P',
+	KNIGHT	= 'N',
+	BISHOP	= 'B',
+	ROOK	= 'R',
+	QUEEN	= 'Q',
+	KING	= 'K'
+};
+
+typedef enum Piece_side_t {
+	DEFAULT,
+	BLACK,
+	WHITE
 };
 
 class Piece
 {
 private:
 	Piece_type_t type;
+	Piece_side_t side;
 	std::pair<char, int> position;
 
 public:
-	Piece(Piece_type_t type = DEFAULT, std::pair<char, int> position = { 'X', 0 }) : type{type}, position{position} {};
+	Piece(
+		Piece_type_t type = Piece_type_t(DEFAULT),
+		Piece_side_t side = Piece_side_t(DEFAULT), 
+		std::pair<char, int> position = { 'X', 0 }) : 
+		type{type},
+		side(side),
+		position{position} {};
 
 	std::pair<char, int> get_position() { return position; };
 	void set_position(std::pair<char, int> _position) { position = _position; };
